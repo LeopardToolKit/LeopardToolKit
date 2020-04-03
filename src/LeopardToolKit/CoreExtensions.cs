a5196b2ce645622996203c9a18a18a161f6fffac
Gary Zhu <ZHUGX@legal.regn.net>
2020-04-03 18:02:25 +0800

    init codes

---
+++ src/LeopardToolKit/CoreExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace LeopardToolKit
+{
+    public static class CoreExtensions
+    {
+        public static T ThrowIfNull<T>(this T obj, string objName)
+        {
+            if (typeof(T).GetTypeInfo().IsValueType || null != obj)
+            {
+                return obj;
+            }
+
+            throw new ArgumentNullException(objName);
+        }
+
+        public static bool IsEmpty(this string str)
+        {
+            return string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
+        }
+    }
+}
