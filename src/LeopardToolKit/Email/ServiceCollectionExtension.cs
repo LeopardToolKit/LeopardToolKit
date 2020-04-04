af02812753d010a0c2a59afb12742e54aad83721
Gary Zhu <390992476@qq.com>
2020-04-04 10:42:19 +0800

    add service collection extension

---
+++ src/LeopardToolKit/Email/ServiceCollectionExtension.cs
@@ -0,0 +1,23 @@
+﻿using LeopardToolKit.Email;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddEmailSender(this IServiceCollection services, Action<EmailOption> configBuilder, ServiceLifetime serviceLifetime = ServiceLifetime.Singleton)
+        {
+            services.TryAdd(ServiceDescriptor.Describe(typeof(IEmailSender), typeof(SmtpEmailSender), serviceLifetime));
+            services.Configure(configBuilder);
+            return services;
+        }
+        public static IServiceCollection AddEmailSender(this IServiceCollection services, IConfiguration configuration, ServiceLifetime serviceLifetime = ServiceLifetime.Singleton)
+        {
+            services.TryAdd(ServiceDescriptor.Describe(typeof(IEmailSender), typeof(SmtpEmailSender), serviceLifetime));
+            services.Configure<EmailOption>(configuration);
+            return services;
+        }
+    }
+}
