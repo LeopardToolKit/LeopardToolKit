5b34f46f9b98173e015799a1bc8a7d12200f083a
Gary Zhu <ZHUGX@legal.regn.net>
2020-04-20 09:03:51 +0800

    fix cache serializer setting

--- src/LeopardToolKit/Cache/DistributedCacheExtension.cs
+++ src/LeopardToolKit/Cache/DistributedCacheExtension.cs
@@ -1,4 +1,5 @@
 ﻿using LeopardToolKit;
+using Newtonsoft.Json;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
@@ -7,37 +8,39 @@ namespace Microsoft.Extensions.Caching.Distributed
 {
     public static class DistributedCacheExtension
     {
-        private static Newtonsoft.Json.JsonSerializerSettings IgnoreSerializerSettings = new Newtonsoft.Json.JsonSerializerSettings() { NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore, DefaultValueHandling = Newtonsoft.Json.DefaultValueHandling.Ignore };
-        public static async Task<T> GetOrSetAsync<T>(this IDistributedCache distributedCache, string key, Func<Task<T>> valueGetter, TimeSpan? absoluteExpirationRelativeToNow = default, CancellationToken token = default)
+        private static JsonSerializerSettings IgnoreSerializerSettings = new Newtonsoft.Json.JsonSerializerSettings() { NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore, DefaultValueHandling = Newtonsoft.Json.DefaultValueHandling.Ignore };
+        public static async Task<T> GetOrSetAsync<T>(this IDistributedCache distributedCache, string key, Func<Task<T>> valueGetter, TimeSpan? absoluteExpirationRelativeToNow = default, CancellationToken token = default, JsonSerializerSettings serializerSettings = default)
         {
+            serializerSettings = serializerSettings ?? IgnoreSerializerSettings;
             var valueString = await distributedCache.GetStringAsync(key, token);
             if (valueString.IsEmpty() && !token.IsCancellationRequested)
             {
 
                 var value = await valueGetter.Invoke();
-                valueString = value?.ToNewtonsoftJson(IgnoreSerializerSettings);
+                valueString = value?.ToNewtonsoftJson(serializerSettings);
                 await distributedCache.SetStringAsync(key, valueString, new DistributedCacheEntryOptions() { AbsoluteExpirationRelativeToNow = absoluteExpirationRelativeToNow }, token);
                 return value;
             }
             else
             {
-                return valueString.DeserializeFromJson<T>(IgnoreSerializerSettings);
+                return valueString.DeserializeFromJson<T>(serializerSettings);
             }
         }
 
-        public static T GetOrSet<T>(this IDistributedCache distributedCache, string key, Func<T> valueGetter, TimeSpan? absoluteExpirationRelativeToNow = default)
+        public static T GetOrSet<T>(this IDistributedCache distributedCache, string key, Func<T> valueGetter, TimeSpan? absoluteExpirationRelativeToNow = default, JsonSerializerSettings serializerSettings)
         {
+            serializerSettings = serializerSettings ?? IgnoreSerializerSettings;
             var valueString = distributedCache.GetString(key);
             if (valueString.IsEmpty())
             {
                 var value = valueGetter.Invoke();
-                valueString = value?.ToNewtonsoftJson(IgnoreSerializerSettings);
+                valueString = value?.ToNewtonsoftJson(serializerSettings);
                 distributedCache.SetString(key, valueString, new DistributedCacheEntryOptions() { AbsoluteExpirationRelativeToNow = absoluteExpirationRelativeToNow });
                 return value;
             }
             else
             {
-                return valueString.DeserializeFromJson<T>(IgnoreSerializerSettings);
+                return valueString.DeserializeFromJson<T>(serializerSettings);
             }
         }
     }
