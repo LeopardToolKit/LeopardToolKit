26b32ef8562c71fed2edc4addf33693eb94cb7ba
Gary Zhu <ZHUGX@legal.regn.net>
2020-08-28 17:27:35 +0800

    remove AddEventHandler

--- src/LeopardToolKit/EventBus/ServiceCollectionExtension.cs
+++ src/LeopardToolKit/EventBus/ServiceCollectionExtension.cs
@@ -38,25 +38,5 @@ namespace Microsoft.Extensions.DependencyInjection
             starter.Start();
             return serviceProvider;
         }
-
-        public static IServiceCollection AddEventHandler<THandler>(this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Transient) where THandler : class, IEventHandler
-        {
-            switch (serviceLifetime)
-            {
-                case ServiceLifetime.Singleton:
-                    services.AddSingleton<THandler>();
-                    break;
-                case ServiceLifetime.Scoped:
-                    services.AddScoped<THandler>();
-                    break;
-                case ServiceLifetime.Transient:
-                    services.AddTransient<THandler>();
-                    break;
-                default:
-                    services.AddTransient<THandler>();
-                    break;
-            }
-            return services;
-        }
     }
 }
